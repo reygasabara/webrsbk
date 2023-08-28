ce1e5fb81ddfe32e22b392e0612327a2382b56a6
arvinzer <subhanaldiansyah3@gmail.com>
2023-08-28 15:11:01 +0800

    perbaiki profile controller

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -12,4 +12,46 @@ class ProfilesController extends Controller
             'submenu' => null,
         ]);
     }
+
+    public function showHistory() {
+        return view('profiles.history', [
+            'title' => 'Profil',
+            'submenu' => 'Sejarah',
+        ]);
+    }
+
+    public function showVisiMisi() {
+        return view('profiles.visiMisi', [
+            'title' => 'Profil',
+            'submenu' => 'Visi dan Misi',
+        ]);
+    }
+
+    public function showStructure() {
+        return view('profiles.structure', [
+            'title'=> 'Profil',
+            'submenu' => 'Struktur Organisasi',
+        ]);
+    }
+
+    public function showSDM() {
+        return view('profiles.sdm', [
+            'title'=> 'Profil',
+            'submenu' => 'SDM',
+        ]);
+    }
+
+    public function showFloorPlan() {
+        return view('profiles.floorPlan', [
+            'title'=> 'Profil',
+            'submenu' => 'Denah Rumah Sakit',
+        ]);
+    }
+
+    public function showLogo() {
+        return view('profiles.logo', [
+            'title'=> 'Profil',
+            'submenu' => 'Logo',
+        ]);
+    }
 }
